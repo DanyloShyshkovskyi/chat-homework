30a8861a3fd66720f078915c3f4616738f495d14
Danylo Shyshkovskyi <danyloshyshkovskyi@gmail.com>
2025-02-08 12:50:19 +0100

    Update useGetChat hook to add current user to chat participants

--- src/widget/chat/api/use-get-chat.ts
+++ src/widget/chat/api/use-get-chat.ts
@@ -2,21 +2,32 @@ import { useEffect, useState } from 'react'
 
 import { ChatInfo } from 'widget/chat/model/types'
 
-import { doc, onSnapshot } from 'firebase/firestore'
+import { useFirebaseAuth } from 'app/providers/with-firebase-auth'
+import { arrayUnion, doc, onSnapshot, updateDoc } from 'firebase/firestore'
 
 import { db } from 'shared/config/firebase'
 
 export const useGetChat = (chatId: string | undefined) => {
   const [chatData, setChatData] = useState<ChatInfo | null>(null)
+  const { user } = useFirebaseAuth()
 
   useEffect(() => {
     if (!chatId) return
+
     const chatRef = doc(db, 'chats', chatId)
     const unsubscribe = onSnapshot(chatRef, (snapshot) => {
       setChatData(snapshot.data() as ChatInfo)
     })
+
+    if (user) {
+      // if user exist patch user id to participants firebase chats
+      updateDoc(chatRef, {
+        participants: arrayUnion(user?.uid),
+      })
+    }
+
     return () => unsubscribe()
-  }, [chatId])
+  }, [chatId, user])
 
   return chatData
 }
